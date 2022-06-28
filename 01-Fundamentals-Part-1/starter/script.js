0a06664ed545436f7889986aab894122289eca7c
robie008 <tajel02@gmail.com>
2022-06-28 20:53:42 +0800

    test

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,35 @@
+// TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+
+// test 1
+// let Dolphins = (96+108+89)/3
+// let Koalas = (88+91+110)/3
+
+// if (Dolphins > Koalas){
+//     console.log('Dolphin wins')
+// }else{
+//     console.log(`Koalas Wins`)
+// }
+
+// test 2
+
+// let Dolphins = 100;
+// let Koalas = 100;
+
+// console.log(`Dolp:${Dolphins} Koa:${Koalas}`);
+
+// if(Dolphins > Koalas && Dolphins > 100){
+//     console.log('dolp wins');
+// }else if(Koalas > Dolphins && Koalas > 100){
+//     console.log('koa wins')
+// }else if(Dolphins === Koalas && Dolphins >= 100 && Koalas >= 100){
+//     console.log('DRAW!')
+// }else if(Dolphins < 100){
+//     console.log('koa win dolp less than 100')
+// }else if(Koalas < 100){
+//     console.log('dol wins koa has less than 100')
+// }
+
+ 
